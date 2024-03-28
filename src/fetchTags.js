7ee687856e73df63e7f7f171703a505f3fb00e89
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-03-28 17:59:35 +0100

    printed in console rJson.items

--- src/fetchTags.js
+++ src/fetchTags.js
@@ -1,4 +1,14 @@
+
+
 export const fetchTags = () =>
-  fetch("https://api.github.com/repos/TanStack/query/contributors").then(
-    (response) => response.json()
-  );
+  fetch("https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
+  ).then( (response) => response.json()
+   )
+.then (rJsn => console.log (rJsn.items) );
+ 
+
+
+  //testowe api
+  //https://api.github.com/repos/TanStack/query/contributors
+  // https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow
+  
