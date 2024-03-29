f9e377a3cc80d26ca1bd06b47d2b08340ce2de77
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-03-29 09:22:13 +0100

    added  rJsn version to try

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import { useQuery } from "@tanstack/react-query";
-import { fetchTags } from "./fetchTags";
+// import { fetchTags } from "./fetchTags";
 
 //"https://jsonplaceholder.typicode.com/trololos"
 
+const fetchTags = () =>
+  fetch("https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
+  ).then( (response) => response.json()
+   )
+.then (rJsn => console.log (rJsn.items) );
+ 
+  //testowe api, z których działa -> mapuje po data
+  //https://api.github.com/repos/TanStack/query/contributors
+  //https://jsonplaceholder.typicode.com/todos
+
+  
+
+
 function App() {
-  const { isLoading, error, data } = useQuery({
+  const { isLoading, error, rJsn } = useQuery({
     queryKey: ["trololo"],
     queryFn: fetchTags,
     // {staleTime:1000*10, },
@@ -15,14 +28,17 @@ function App() {
     <>
       <div className="app">
         placeholer
-        {/* {" "}
-        {data.items.map((trololo) => (
+        {" "}
+        
+{/* //trzeba się dokopać do arraya, żeby można było mapować */}
+{/* //ale nie działa tą metodą  */}
+      {rJsn.items.map((trololo) => (
           <div>
             {" "}
             <h1>Name:{trololo.count}</h1>
             <h2>Count:{trololo.name}</h2>
           </div>
-        ))} */}
+        ))} 
       </div>
     </>
   );
--- src/fetchTags.js
+++ src/fetchTags.js
@@ -1,14 +1,2 @@
 
 
-export const fetchTags = () =>
-  fetch("https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
-  ).then( (response) => response.json()
-   )
-.then (rJsn => console.log (rJsn.items) );
- 
-
-
-  //testowe api
-  //https://api.github.com/repos/TanStack/query/contributors
-  // https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow
-  
