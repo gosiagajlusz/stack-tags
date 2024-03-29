14d82b3e42cc5761122c5d31e227a8bb9390715f
Małgorzata Gajlusz <gosiagajlusz@gmail.com>
2024-03-29 16:12:16 +0100

    added conditional chaining and removed console logs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import "./App.css";
 import { useQuery } from "@tanstack/react-query";
 
 //"https://jsonplaceholder.typicode.com/trololos"
-
 // const fetchTags =
-
 // .then ((data) => return data)
 // .then(function (rJsn) {
 //   let xd = rJsn.items;
 // });
-
 //testowe api, z których działa -> mapuje po data
 //https://api.github.com/repos/TanStack/query/contributors
 //https://jsonplaceholder.typicode.com/todos
@@ -23,24 +20,22 @@ function App() {
     queryFn: () =>
       fetch(
         "https://api.stackexchange.com/2.3/tags?order=desc&sort=popular&site=stackoverflow"
-      )
-        .then((response) => response.json())
-        .then((data) => console.log(data.items)),
-
+      ).then((response) => response.json()),
+    // .then((data) => console.log(data.items)),
     // .then((data.items.array.forEach(element => {
     //   console.log(element.name)
     // })))
-
     // fetchTags,
     // {staleTime:1000*10, },
   });
+
+  // console.log(data.items);
 
   return (
     <>
       <div className="app">
         placeholer{" "}
-        
-        {data.items.map((item) => (
+        {data?.items.map((item) => (
           <div>
             {" "}
             <h1>Name:{item.name}</h1>
